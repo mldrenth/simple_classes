971f8cb7d0cadf93bcc8ed87be4c5166429178a7
Marijke Drenth <drenth.marijke@googlemail.com>
2022-01-31 18:05:05 +0200

    Add calculator tests and class

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    Calculator calculator;
+    @Before
+    public void before(){
+        calculator = new Calculator();
+    }
+    @Test
+    public void add5to7equals12(){
+        assertEquals(12, calculator.add(5,7));
+    }
+
+    @Test
+    public void subtract12from7equals5(){
+        assertEquals(5, calculator.subtract(12,7));
+    }
+
+    @Test
+    public void multiply2with6equals12(){
+        assertEquals(12, calculator.multiply(2,6));
+    }
+
+    @Test
+    public void divide12by6equals2(){
+        assertEquals(2.00, calculator.divide(12.00, 6.00), 0.00);
+    }
+}
